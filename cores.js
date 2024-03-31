f4c44c117601c5826dd6cef882a2ef3841f6df58
moh4b-z <mohammadsalim10000@gmail.com>
2024-03-31 19:53:07 -0300

    mudei

---
+++ cores.js
@@ -0,0 +1,15 @@
+// Este script é necessário apenas se você deseja alternar as cores dinamicamente
+
+// A paleta de cores
+const cores = ['#99cee6', '#375899'];
+
+// Obtém o elemento h1
+const titulo = document.getElementById('titulo');
+
+// Função para alternar entre as cores da paleta
+function alternarCores() {
+    titulo.style.color = cores[Math.floor(Math.random() * cores.length)];
+}
+
+// Chama a função para alternar cores em intervalos regulares
+setInterval(alternarCores, 5000); // Altera a cor a cada 5 segundos
